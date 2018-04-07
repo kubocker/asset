ebf292da5e0b2dcc9ea27d72dc4c3278bb3276c1
kubocker <kubocker@gmail.com>
2018-04-07 15:07:39 +0900

    add Numbers table orm

--- api/src/entity/Numbers.ts
+++ api/src/entity/Numbers.ts
@@ -1,6 +1,25 @@
-import {Entity} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
 @Entity()
 export class Numbers {
+    @PrimaryGeneratedColumn()
+    id: number;
 
+    @Column()
+    title: string;
+
+    @Column()
+    date: string;
+
+    @Column()
+    numbers3: string;
+
+    @Column()
+    numbers4: string;
+
+    @Column()
+    calendar: string;
+
+    @Column()
+    week: string;
 }

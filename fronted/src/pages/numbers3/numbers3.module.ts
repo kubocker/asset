aa5f89826a399c1a410c76c09ff947de4c965d94
kubocker <kubocker@gmail.com>
2018-04-08 17:19:30 +0900

    add front page numbers3

---
+++ fronted/src/pages/numbers3/numbers3.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Numbers3Page } from './numbers3';
+
+@NgModule({
+  declarations: [
+    Numbers3Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Numbers3Page),
+  ],
+})
+export class Numbers3PageModule {}

c9bcf528e216a8e8a32aad427b9ef98a4c7bbad7
kubocker <kubocker@gmail.com>
2018-04-08 16:25:53 +0900

    add api

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,10 @@ import * as bodyParser from "body-parser";
 import {Request, Response} from "express";
 import {Routes} from "./routes";
 import {User} from "./entity/User";
+import { arr } from "./scripts/Numbers";
+import { arr2 } from "./scripts/Loto";
+import { Numbers } from "./entity/Numbers";
+import { Loto } from "./entity/Loto";
 
 createConnection().then(async connection => {
 
@@ -32,16 +36,16 @@ createConnection().then(async connection => {
     app.listen(3000);
 
     // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
+    //await connection.manager.save(connection.manager.create(User, {
+    //    firstName: "Timber",
+    //    lastName: "Saw",
+    //    age: 27
+    //}));
+    //await connection.manager.save(connection.manager.create(User, {
+    //    firstName: "Phantom",
+    //    lastName: "Assassin",
+    //    age: 24
+    //}));
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 

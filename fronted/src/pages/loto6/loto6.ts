36596a6bf1959ba34025dce2caf4c919ec04e624
kubocker <kubocker@gmail.com>
2018-04-08 17:19:06 +0900

    add loto6

---
+++ fronted/src/pages/loto6/loto6.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the Loto6Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-loto6',
+  templateUrl: 'loto6.html',
+})
+export class Loto6Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Loto6Page');
+  }
+
+}

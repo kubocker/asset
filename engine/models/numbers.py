00654f5ed256c0018e836327353bd88b8a4dc1b2
kubocker <kubocker@gmail.com>
2018-05-10 16:01:45 +0900

    new file add

---
+++ engine/models/numbers.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+__all__ = [
+    "Numbers",
+    "Loto"
+]
+
+
+class Numbers(object):
+
+    def __init__(self, no_:str):
+        pass
+
+    def __call__(self, type_:str) -> tuple:
+        pass
+
+    def update(self):
+        pass
+
+
+class Loto(object):
+
+    def __init__(self, no_:str):
+        pass
+
+    def __call__(self, type_:str) -> tuple:
+        pass
+
+    def update(self):
+        pass

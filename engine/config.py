23c2b051f7f1da58241310dd7a46ef6a31c27f33
kubocker <kubocker@gmail.com>
2018-06-17 16:31:05 +0900

    add data loto6 to analyze

--- engine/config.py
+++ engine/config.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+# coding: utf8
 
 from invincible import models as g
 
 NUMBERS         = g.numbers.MODELS
 NUMBERS3        = g.numbers3.DATA_NUMBERS3
 NUMBERS4        = g.numbers4.DATA_NUMBERS4
+LOTO6           = g.loto6.MODELS
+LOTO6_RESULT    = g.loto6_result.DATA_LOTO6_RESULT
 NUMBERS3_RESULT = g.numbers3_result.DATA_NUMBERS3_RESULT
 NUMBERS4_RESULT = g.numbers4_result.DATA_NUMBERS4_RESULT
--- engine/invincible/models/__init__.py
+++ engine/invincible/models/__init__.py
@@ -5,3 +5,5 @@ from . import numbers3
 from . import numbers3_result
 from . import numbers4
 from . import numbers4_result
+from . import loto6
+from . import loto6_result

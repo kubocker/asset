a877e523aaba4a2a7387fae2c02c7a48422f07a5
kubocker <kubocker@gmail.com>
2018-07-08 22:00:23 +0900

    add models

---
+++ engine/invincible/models/numbers3_vectors.py
@@ -0,0 +1,16 @@
+# code: utf8
+
+VECTOR_NUMBERS3 = [
+    { "name": "赤口", "type": "+", "vectors": [0, 2, 3] },
+    { "name": "赤口", "type": "-", "vectors": [1, 4, 5] },
+    { "name": "友引", "type": "+", "vectors": [] },
+    { "name": "友引", "type": "-", "vectors": [] },
+    { "name": "先勝", "type": "+", "vectors": [] },
+    { "name": "先勝", "type": "-", "vectors": [] },
+    { "name": "先負", "type": "+", "vectors": [] },
+    { "name": "先負", "type": "-", "vectors": [] },
+    { "name": "仏滅", "type": "+", "vectors": [] },
+    { "name": "仏滅", "type": "-", "vectors": [] },
+    { "name": "大安", "type": "+", "vectors": [] },
+    { "name": "大安", "type": "-", "vectors": [] },
+]
---
+++ engine/invincible/models/numbers4_vectors.py
@@ -0,0 +1,16 @@
+# code: utf8
+
+VECTOR_NUMBERS4 = [
+    { "name": "赤口", "type": "+", "vectors": [1, 2, 3] },
+    { "name": "赤口", "type": "-", "vectors": [0, 4, 5] },
+    { "name": "友引", "type": "+", "vectors": [] },
+    { "name": "友引", "type": "-", "vectors": [] },
+    { "name": "先勝", "type": "+", "vectors": [] },
+    { "name": "先勝", "type": "-", "vectors": [] },
+    { "name": "先負", "type": "+", "vectors": [] },
+    { "name": "先負", "type": "-", "vectors": [] },
+    { "name": "仏滅", "type": "+", "vectors": [] },
+    { "name": "仏滅", "type": "-", "vectors": [] },
+    { "name": "大安", "type": "+", "vectors": [] },
+    { "name": "大安", "type": "-", "vectors": [] },
+]

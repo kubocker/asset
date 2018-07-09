01903c11ade2554e13a9846d3ecece93be601498
kubocker <kubocker@gmail.com>
2018-07-09 21:07:33 +0900

    fix

--- engine/invincible/models/__init__.py
+++ engine/invincible/models/__init__.py
@@ -6,6 +6,6 @@ from . import numbers3_result
 from . import numbers3_vectors
 from . import numbers4
 from . import numbers4_result
-from . import numbers3_vectors
+from . import numbers4_vectors
 from . import loto6
 from . import loto6_result
